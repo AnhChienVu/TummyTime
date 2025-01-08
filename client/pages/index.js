e7d31bc3d4e00f29a7dc8ad1379d3b0da1c5c562
Anh Chien Vu <vuanhchien003@gmail.com>
2025-01-07 19:05:24 -0500

    create UI's Login form

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,11 +1,58 @@
 import React, { useEffect, useState } from "react";
+import { Button, Form, Container, Row, Col } from "react-bootstrap";
+import styles from "./index.module.css";
+import Link from "next/link";
 
-export default function Index() {
-  useEffect(() => {
-    fetch("http://localhost:8080/")
-      .then((res) => res.json())
-      .then((data) => console.log(data));
-  }, []);
+export default function Home() {
+  // useEffect(() => {
+  //   fetch("http://localhost:8080/")
+  //     .then((res) => res.json())
+  //     .then((data) => console.log(data));
+  // }, []);
 
-  return <div>index</div>;
+  return (
+    <Container className={styles.container} fluid>
+      <div className={styles.formContainer}>
+        <Form className={styles.form}>
+          <p className={styles.title}>Welcome back !</p>
+
+          <Form.Group className="mb-3" controlId="emailLogin">
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              className={styles.formControl}
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="passwordLogin">
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              className={styles.formControl}
+            />
+          </Form.Group>
+
+          <Button
+            variant="primary"
+            type="submit"
+            className={styles.loginButton}
+          >
+            Login
+          </Button>
+
+          <div className="mt-3">
+            <Link href="/" className={styles.link}>
+              Forgot password ?
+            </Link>
+            <p>
+              Dont have an account ?{" "}
+              <Link href="/" className={styles.link}>
+                Sign up
+              </Link>
+            </p>
+          </div>
+        </Form>
+      </div>
+    </Container>
+  );
 }
