a4c680c1c0e673ab35b315482f315c2e6b3a4e90
Anh Chien Vu <vuanhchien003@gmail.com>
2025-01-14 13:55:49 -0500

    hide sensitive information such as password when logging under LOG_LEVEL='info'

--- server/src/utils/logger.js
+++ server/src/utils/logger.js
@@ -1,5 +1,11 @@
 // Use `info` as our standard log level if not specified
-const options = { level: process.env.LOG_LEVEL || 'info' };
+const options = {
+  level: process.env.LOG_LEVEL || 'info',
+  redact: {
+    paths: ['user.password'],
+    censor: '[REDACTED]',
+  },
+};
 
 // If we're doing `debug` logging, make the logs easier to read
 if (options.level === 'debug') {
