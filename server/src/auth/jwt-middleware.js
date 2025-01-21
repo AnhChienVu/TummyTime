9ebe98f61bc74c4600f0f9733d4e4353fae51b91
Anh Chien Vu <vuanhchien003@gmail.com>
2025-01-20 22:55:36 -0500

    1. Separate the flow of using cognito or local postgres database to import authentication module by using AUTH_METHOD env 2. Using passport-jwt for authentication 3. Delete basic-auth.js file because I used passport jwt authentication instead

--- server/src/auth/jwt-middleware.js
+++ server/src/auth/jwt-middleware.js
@@ -1,20 +1,47 @@
-const jwt = require('jsonwebtoken');
+const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
+const logger = require('../utils/logger');
 const secret = 'jwt_secret_key';
+const jwt = require('jsonwebtoken');
 
-const jwtMiddleware = (req, res, next) => {
-  const token = req.headers.authorization?.split(' ')[1];
+module.exports.strategy = () => {
+  return new JwtStrategy(
+    {
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: secret,
+      passReqToCallback: true,
+    },
+    (req, jwtPayload, done) => {
+      try {
+        // Debug the incoming request headers
+        logger.info({ headers: req.headers }, 'Request Headers');
+        // Extract token using the correct method
+        const tokenExtractor = ExtractJwt.fromAuthHeaderAsBearerToken();
+        const token = tokenExtractor(req);
 
-  if (!token) {
-    return res.status(401).json({ message: 'Unauthorized' });
-  }
+        if (!token) {
+          throw new Error('No token provided');
+        }
 
-  try {
-    const decoded = jwt.verify(token, secret);
-    req.user = decoded;
-    next();
-  } catch (error) {
-    return res.status(401).json({ message: 'Invalid token' });
-  }
+        logger.info({ token }, 'Extracted token from request headers');
+
+        // Verify the token
+        const decoded = jwt.verify(token, secret);
+        logger.debug(decoded, 'Verified user token');
+
+        // Validate the decoded payload
+        if (typeof decoded !== 'object' || !decoded.email) {
+          throw new Error('Invalid token payload');
+        }
+
+        decoded.email = decoded.email.trim();
+        done(null, decoded.email);
+      } catch (error) {
+        logger.error({ error }, 'Error verifying user token');
+        done(null, false); // Call done with `false` to indicate authentication failure
+      }
+    }
+  );
 };
 
-module.exports = jwtMiddleware;
+const authorize = require('./auth-middleware');
+module.exports.authenticate = () => authorize('jwt');
