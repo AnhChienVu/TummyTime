9a52e6e9506a634170006f231de0c749ee9d88c3
Xi Chen <xchen339@myseneca.ca>
2025-02-03 20:57:45 -0500

    Implement a rich text editor

---
+++ client/components/RichTextEditor/RichTextEditor.jsx
@@ -0,0 +1,50 @@
+// components/RichTextEditor/RichTextEditor.jsx
+import React, { useEffect, useRef } from "react";
+import "quill/dist/quill.snow.css";
+import Quill from "quill/core";
+import Toolbar from "quill/modules/toolbar";
+import Snow from "quill/themes/snow";
+import Bold from "quill/formats/bold";
+import Italic from "quill/formats/italic";
+import Header from "quill/formats/header";
+
+Quill.register({
+  "modules/toolbar": Toolbar,
+  "themes/snow": Snow,
+  "formats/bold": Bold,
+  "formats/italic": Italic,
+  "formats/header": Header,
+});
+
+const RichTextEditor = ({ value, onChange }) => {
+  const editorRef = useRef(null);
+  const quillRef = useRef(null);
+
+  useEffect(() => {
+    if (editorRef.current && !quillRef.current) {
+      quillRef.current = new Quill(editorRef.current, {
+        theme: "snow",
+      });
+
+      quillRef.current.on("text-change", () => {
+        onChange(quillRef.current.root.innerHTML);
+      });
+    }
+
+    return () => {
+      if (quillRef.current) {
+        quillRef.current.off("text-change");
+      }
+    };
+  }, [onChange]);
+
+  useEffect(() => {
+    if (quillRef.current) {
+      quillRef.current.root.innerHTML = value;
+    }
+  }, [value]);
+
+  return <div ref={editorRef} id="editor" />;
+};
+
+export default RichTextEditor;
