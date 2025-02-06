31eaa2a32ec2fc66f8cef5557f65693882d646b9
Xi Chen <xchen339@myseneca.ca>
2025-02-06 12:03:12 -0500

    Replace addBaby.js, getBabyProfiles.js

---
+++ server/src/routes/api/baby/babyProfile/addBabyProfile.js
@@ -0,0 +1,30 @@
+// src/routes/api/addBaby.js
+const pool = require("../../../../../database/db");
+const {
+  createSuccessResponse,
+  createErrorResponse,
+} = require("../../../../utils/response");
+
+module.exports = async (req, res) => {
+  const userId = req.params.user_id;
+  const { first_name, last_name, gender, weight } = req.body;
+
+  try {
+    const newBaby = await pool.query(
+      "INSERT INTO Baby (first_name, last_name, gender, weight) VALUES ($1, $2, $3, $4) RETURNING *",
+      [first_name, last_name, gender, weight]
+    );
+
+    await pool.query(
+      "INSERT INTO user_baby (user_id, baby_id) VALUES ($1, $2)",
+      [userId, newBaby.rows[0].baby_id]
+    );
+
+    return res.json(createSuccessResponse(newBaby.rows[0]));
+  } catch (error) {
+    console.error("Database query error:", error);
+    return res
+      .status(500)
+      .json(createErrorResponse(500, "Internal server error"));
+  }
+};
