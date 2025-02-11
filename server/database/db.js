78dd85838758ec7574c513214baa2eee5554ba02
Anh Chien Vu <vuanhchien003@gmail.com>
2025-02-11 14:19:46 -0500

    Configure connection to Supabase DB

--- server/database/db.js
+++ server/database/db.js
@@ -1,12 +1,31 @@
 require('dotenv').config();
 const Pool = require('pg').Pool;
 
+// For local PostgresDB
+// const pool = new Pool({
+//   user: 'postgres',
+//   password: process.env.POSTGRES_PASSWORD,
+//   host: 'localhost',
+//   port: 5432,
+//   database: 'TummyTime',
+// });
+
+// For Supabase PostgresDB
 const pool = new Pool({
-  user: 'postgres',
-  password: process.env.POSTGRES_PASSWORD,
-  host: 'localhost',
-  port: 5432,
-  database: 'TummyTime',
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
+
+// Test database connection
+pool
+  .connect()
+  .then(() => {
+    console.log('Connected to database');
+  })
+  .catch((err) => {
+    console.error('Error connecting to database', err);
+  });
 
 module.exports = pool;
