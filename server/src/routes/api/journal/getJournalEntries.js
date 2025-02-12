ef78cedfe7bbe33735e22bd21d88801b5f944134
Anh Chien Vu <vuanhchien003@gmail.com>
2025-02-12 14:52:26 -0500

    Add authorization headers and remove the fixed user Id

--- server/src/routes/api/journal/getJournalEntries.js
+++ server/src/routes/api/journal/getJournalEntries.js
@@ -1,28 +1,30 @@
 // src/routes/api/journal/getJournalEntry.js
-const express = require("express");
-const logger = require("../../../utils/logger");
+const express = require('express');
+const logger = require('../../../utils/logger');
 const {
   createSuccessResponse,
   createErrorResponse,
-} = require("../../../utils/response");
-const pool = require("../../../../database/db");
+} = require('../../../utils/response');
+const pool = require('../../../../database/db');
 
 // GET /v1/getJournalEntries
 module.exports = async (req, res) => {
   try {
     const userId = req.params.user_id;
-    const query = "SELECT * FROM journalentry WHERE user_id = $1";
+    const query = 'SELECT * FROM journalentry WHERE user_id = $1';
     const { rows } = await pool.query(query, [userId]);
 
     if (rows.length === 0) {
       return res
         .status(404)
-        .json(createErrorResponse("No journal entries found"));
+        .json(
+          createErrorResponse('No journal entries found. Try to create one')
+        );
     }
 
     res.status(200).json(createSuccessResponse(rows));
   } catch (error) {
-    logger.error("Error retrieving journal entries:", error);
-    res.status(500).json(createErrorResponse("Internal server error"));
+    logger.error('Error retrieving journal entries:', error);
+    res.status(500).json(createErrorResponse('Internal server error'));
   }
 };
