7abb4cab92917daffa95fd20e2ce2c8462cc62bc
Xi Chen <xchen339@myseneca.ca>
2025-02-25 17:49:55 -0500

    Redesign footer

--- client/components/Footer/Footer.jsx
+++ client/components/Footer/Footer.jsx
@@ -1,13 +1,38 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col, Nav } from "react-bootstrap";
 import styles from "./Footer.module.css";
 
 function Footer() {
   return (
     <Container className={styles.footer} fluid>
-      <p className="mb-0">
-        &copy; {new Date().getFullYear()} Baby Page. All rights reserved.
-      </p>
+      {/* <Row className={`${styles.footerRow} py-3`}>
+        <Col md={4}>
+          <h5>About Us</h5>
+          <p>Supporting parents and caregivers in their journey through parenthood.</p>
+        </Col>
+        <Col md={4}>
+          <h5>Quick Links</h5>
+          <ul className="list-unstyled">
+            <li><Nav.Link href="/">Home</Nav.Link></li>
+            <li><Nav.Link href="/resources">Resources</Nav.Link></li>
+            <li><Nav.Link href="/contact">Contact</Nav.Link></li>
+          </ul>
+        </Col>
+        <Col md={4}>
+          <h5>Contact Info</h5>
+          <ul className="list-unstyled">
+            <li>Email: info@babypage.com</li>
+            <li>Phone: (123) 456-7890</li>
+          </ul>
+        </Col>
+      </Row> */}
+      <Row className={`${styles.copyrightRow} py-2 border-top`}>
+        <Col className="text-center">
+          <small className="text-muted">
+            &copy; {new Date().getFullYear()} Tummy Time. All rights reserved.
+          </small>
+        </Col>
+      </Row>
     </Container>
   );
 }
