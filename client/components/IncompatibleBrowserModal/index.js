7668f10ebd21e597a5de5fc079d740b297c70f7a
Xi Chen <xchen339@myseneca.ca>
2025-03-14 18:16:43 -0400

    Use CSS styling

--- client/components/IncompatibleBrowserModal/index.js
+++ client/components/IncompatibleBrowserModal/index.js
@@ -5,38 +5,51 @@
 import React from "react";
 import { Modal, Button } from "react-bootstrap";
 import { useTranslation } from "next-i18next";
+import styles from "./incompatibleBrowserModal.module.css";
 
 const IncompatibleBrowserModal = ({ show, onHide }) => {
   const { t } = useTranslation("common");
 
   return (
-    <Modal show={show} onHide={onHide} centered>
-      <Modal.Header closeButton>
-        <Modal.Title>{t("Browser Configuration Required")}</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <p>{t("To use speech recognition in Firefox:")}</p>
-        <ol style={{ textAlign: "left" }}>
-          <li>
-            {t("Type")} <strong>about:config</strong> {t("in your address bar")}
-          </li>
-          <li>{t("Accept the warning")}</li>
-          <li>
-            {t("Search for")}{" "}
-            <strong>media.webspeech.recognition.enable</strong>
-          </li>
-          <li>
-            {t("Set it to")} <strong>true</strong>
-          </li>
-          <li>{t("Restart Firefox")}</li>
-        </ol>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={onHide}>
-          {t("Close")}
-        </Button>
-      </Modal.Footer>
-    </Modal>
+    <div className={show ? styles.modalOverlay : ""}>
+      <Modal
+        show={show}
+        onHide={onHide}
+        centered
+        dialogClassName={styles.modalContent}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>{t("Browser Configuration Required")}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>{t("To use speech recognition in Firefox:")}</p>
+          <ol style={{ textAlign: "left" }}>
+            <li>
+              {t("Type")} <strong>about:config</strong>{" "}
+              {t("in your address bar")}
+            </li>
+            <li>{t("Accept the warning")}</li>
+            <li>
+              {t("Search for")}{" "}
+              <strong>media.webspeech.recognition.enable</strong>
+            </li>
+            <li>
+              {t("Set it to")} <strong>true</strong>
+            </li>
+            <li>{t("Restart Firefox")}</li>
+          </ol>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            variant="outline-primary"
+            onClick={onHide}
+            className={styles.closeButton}
+          >
+            {t("Close")}
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
   );
 };
 
