a32b6d3042b92052329aa21e4b1c11972c1a6aa8
Xi Chen <xchen339@myseneca.ca>
2025-04-08 15:15:25 -0400

    Remove the `/me` route

--- server/src/routes/api/index.js
+++ server/src/routes/api/index.js
@@ -54,8 +54,6 @@ router.get(
 );
 
 //************ /user routes ************
-router.get('/user/me', authenticate(), require('./user/me').getUserDetails);
-
 router.get('/user', authenticate(), require('./user/getUser').getUserById);
 
 router.put('/user/:id', authenticate(), require('./user/putUser').updateUserById);
@@ -249,6 +247,11 @@ router.delete(
   authenticate(),
   require('./profilePicture/deleteProfilePicture')
 );
+
+// ************ /bookmarks routes ************
+// router.get('/forum/bookmarks', authenticate(), require('./forum/bookmarks/getBookmarks'));
+
+// router.post('/forum/bookmarks/:postId', authenticate(), require('./forum/bookmarks/addBookmark'));
 
 // Testing the authentication middleware
 // router.get('/test', authenticate(), require('./test'));
