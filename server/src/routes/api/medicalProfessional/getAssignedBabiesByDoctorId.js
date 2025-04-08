49df0048873d410e5e2ee40912393cf34e7e9780
Anh Chien Vu <vuanhchien003@gmail.com>
2025-04-08 18:36:54 -0400

    Fix the route get babies and parent info from doctor

--- server/src/routes/api/medicalProfessional/getAssignedBabiesByDoctorId.js
+++ server/src/routes/api/medicalProfessional/getAssignedBabiesByDoctorId.js
@@ -1,10 +1,7 @@
 // src/routes/api/medicalProfessional/getAssignedBabiesByDoctorId.js
 
 const logger = require('../../../utils/logger');
-const {
-  createSuccessResponse,
-  createErrorResponse,
-} = require('../../../utils/response');
+const { createSuccessResponse, createErrorResponse } = require('../../../utils/response');
 const pool = require('../../../../database/db');
 
 module.exports.getAssignedBabiesByDoctorId = async function (req, res) {
@@ -13,25 +10,50 @@ module.exports.getAssignedBabiesByDoctorId = async function (req, res) {
     const query = `SELECT * FROM doctor_baby WHERE doctor_id = $1`;
     const result = await pool.query(query, [doctorId]);
     if (result.rows.length === 0) {
-      return res
-        .status(404)
-        .json(createErrorResponse('No assigned babies found for this doctor'));
+      return res.status(404).json(createErrorResponse('No assigned babies found for this doctor'));
     }
     const babyIds = result.rows.map((row) => row.baby_id);
+
+    // Query to get baby details along with parent information
     const babyInfos = await pool.query(
-      `SELECT * FROM baby WHERE baby_id = ANY($1::int[])`,
+      `SELECT
+        b.baby_id,
+        b.first_name AS baby_first_name,
+        b.last_name AS baby_last_name,
+        b.gender,
+        b.weight,
+        b.height,
+        b.birthdate,
+        u.user_id AS parent_id,
+        u.first_name AS parent_first_name,
+        u.last_name AS parent_last_name
+       FROM baby b
+       INNER JOIN user_baby ub ON b.baby_id = ub.baby_id
+       INNER JOIN users u ON ub.user_id = u.user_id
+       WHERE b.baby_id = ANY($1::int[])`,
       [babyIds]
     );
-    console.log('Hi', babyInfos);
+
+    if (babyInfos.rows.length === 0) {
+      return res.status(404).json(createErrorResponse('No baby information found'));
+    }
+
     return res.status(200).json(
       createSuccessResponse({
-        babies: babyInfos.rows,
+        babies: babyInfos.rows.map((row) => ({
+          baby_id: row.baby_id,
+          baby_name: `${row.baby_first_name} ${row.baby_last_name}`,
+          gender: row.gender,
+          weight: row.weight,
+          height: row.height,
+          birthdate: row.birthdate,
+          parent_id: row.parent_id,
+          parent_name: `${row.parent_first_name} ${row.parent_last_name}`,
+        })),
       })
     );
   } catch (error) {
     logger.error(error);
-    return res
-      .status(500)
-      .json(createErrorResponse('Error fetching assigned babies'));
+    return res.status(500).json(createErrorResponse('Error fetching assigned babies'));
   }
 };
