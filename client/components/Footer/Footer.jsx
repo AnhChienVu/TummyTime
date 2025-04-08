8daffb25aeb0d27a374c8ec9454fb8d48a57f70b
Henry Luong <itchanhtin.mobile@gmail.com>
2025-04-07 20:28:55 -0400

    add Token Expiration Button

--- client/components/Footer/Footer.jsx
+++ client/components/Footer/Footer.jsx
@@ -1,37 +1,51 @@
 import React from "react";
-import { Container, Row, Col, Nav } from "react-bootstrap";
+import { Container, Row, Col, Nav, Button } from "react-bootstrap";
 import styles from "./Footer.module.css";
 
-function Footer() {
+function Footer({ onSimulateExpire }) {
   return (
     <Container className={styles.footer} fluid>
       {/* <Row className={`${styles.footerRow} py-3`}>
-        <Col md={4}>
-          <h5>About Us</h5>
-          <p>Supporting parents and caregivers in their journey through parenthood.</p>
-        </Col>
-        <Col md={4}>
-          <h5>Quick Links</h5>
-          <ul className="list-unstyled">
-            <li><Nav.Link href="/">Home</Nav.Link></li>
-            <li><Nav.Link href="/resources">Resources</Nav.Link></li>
-            <li><Nav.Link href="/contact">Contact</Nav.Link></li>
-          </ul>
-        </Col>
-        <Col md={4}>
-          <h5>Contact Info</h5>
-          <ul className="list-unstyled">
-            <li>Email: info@babypage.com</li>
-            <li>Phone: (123) 456-7890</li>
-          </ul>
-        </Col>
-      </Row> */}
+      <Col md={4}>
+        <h5>About Us</h5>
+        <p>Supporting parents and caregivers in their journey through parenthood.</p>
+      </Col>
+      <Col md={4}>
+        <h5>Quick Links</h5>
+        <ul className="list-unstyled">
+          <li><Nav.Link href="/">Home</Nav.Link></li>
+          <li><Nav.Link href="/resources">Resources</Nav.Link></li>
+          <li><Nav.Link href="/contact">Contact</Nav.Link></li>
+        </ul>
+      </Col>
+      <Col md={4}>
+        <h5>Contact Info</h5>
+        <ul className="list-unstyled">
+          <li>Email: info@babypage.com</li>
+          <li>Phone: (123) 456-7890</li>
+        </ul>
+      </Col>
+    </Row> */}
+
+      {/* Copyright Row */}
       <Row className={`${styles.copyrightRow} py-2 border-top`}>
         <Col className="text-center">
           <small className="text-muted">
             &copy; {new Date().getFullYear()} Tummy Time. All rights reserved.
           </small>
         </Col>
+      </Row>
+
+      {/* Simulate Token Expiration Button */}
+      <Row className="justify-content-center mt-2">
+        <Button
+          variant="outline-secondary"
+          size="sm"
+          style={{ opacity: 0.6 }}
+          onClick={onSimulateExpire}
+        >
+          Simulate Token Expiration
+        </Button>
       </Row>
     </Container>
   );
