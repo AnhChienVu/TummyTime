33eb542e5cc6a01b5a7164085758376e8ce69399
Anh Chien Vu <vuanhchien003@gmail.com>
2025-04-08 22:27:00 -0400

    Fix the params passed in checkBabyBelongsToUser

--- server/src/routes/api/index.js
+++ server/src/routes/api/index.js
@@ -39,7 +39,7 @@ router.get('/baby/:id/getFeedingSchedules', authenticate(), require('./baby/getF
 router.get('baby/:id/getLatestFeed', authenticate(), require('./baby/getLatestFeed'));
 
 router.put(
-  '/baby/:id/updateFeedingSchedule/:mealId',
+  '/baby/:babyId/updateFeedingSchedule/:mealId',
   authenticate(),
   require('./baby/updateFeedingSchedule')
 );
