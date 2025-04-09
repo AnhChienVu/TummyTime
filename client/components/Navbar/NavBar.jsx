f8d60fc5683679342d9b816a960e6677899c89cb
Xi Chen <xchen339@myseneca.ca>
2025-04-09 18:33:57 -0400

    Navbar items are fixed and will not move as screen size changes

--- client/components/Navbar/NavBar.jsx
+++ client/components/Navbar/NavBar.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Navbar, Nav } from "react-bootstrap";
 import Image from 'next/image';
 import styles from "./NavBar.module.css";
 import { useTranslation } from "next-i18next";
@@ -24,35 +24,31 @@ function NavBar() {
   };
 
   return (
-    <Navbar expand="lg" fixed="top" className={styles.navbar}>
-      <Container>
-        <Navbar.Brand href="/dashboard">
-          <Image
-            src="/logo.png"
-            alt="Tummy Time Logo"
-            width={40}
-            height={40}
-            priority
-          />
-          <span className="ms-2">Tummy Time</span>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            <Nav.Link href="/aboutUs">{t("About Us")}</Nav.Link>
-            <Nav.Link href="/faq">FAQ</Nav.Link>
-            {isAuthenticated ? (
-              <Nav.Link href="/" onClick={handleLogoutBtn}>
-                {t("Log out")}
-              </Nav.Link>
-            ) : (
-              <Nav.Link as={Link} href="/login" locale={locale}>
-                {t("Login")}
-              </Nav.Link>
-            )}
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
+    <Navbar expand="lg" fixed="top" className={styles.navContainer}>
+      <Link href="/dashboard" className={styles.brand}>
+        <Image
+          src="/logo.png"
+          alt="Tummy Time Logo"
+          width={40}
+          height={40}
+          priority
+          className={styles.logo}
+        />
+        <span className={styles.brandName}>Tummy Time</span>
+      </Link>
+      <div className={styles.topNav}>
+        <Nav.Link href="/aboutUs">{t("About Us")}</Nav.Link>
+        <Nav.Link href="/faq">FAQ</Nav.Link>
+        {isAuthenticated ? (
+          <Nav.Link href="/" onClick={handleLogoutBtn}>
+            {t("Log out")}
+          </Nav.Link>
+        ) : (
+          <Nav.Link as={Link} href="/login" locale={locale}>
+            {t("Login")}
+          </Nav.Link>
+        )}
+      </div>
     </Navbar>
   );
 }
